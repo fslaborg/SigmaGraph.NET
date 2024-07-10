a5a1f90c6bf74be1aa1c6dfdfa79acb0edefb54f
Christopher Lux <lux-christopher@web.de>
2024-07-10 16:40:45 +0200

    Fix documentation

--- build/DocumentationTasks.fs
+++ build/DocumentationTasks.fs
@@ -9,27 +9,27 @@ open BlackFox.Fake
 let buildDocs = BuildTask.create "BuildDocs" [build] {
     printfn "building docs with stable version %s" stableVersionTag
     runDotNet 
-        (sprintf "fsdocs build --eval --clean --properties Configuration=Release --parameters fsdocs-package-version %s" stableVersionTag)
+        (sprintf "fsdocs build --clean --properties Configuration=Release --parameters fsdocs-package-version %s" stableVersionTag)
         "./"
 }
 
 let buildDocsPrerelease = BuildTask.create "BuildDocsPrerelease" [setPrereleaseTag; build] {
     printfn "building docs with prerelease version %s" prereleaseTag
     runDotNet 
-        (sprintf "fsdocs build --eval --clean --properties Configuration=Release --parameters fsdocs-package-version %s" prereleaseTag)
+        (sprintf "fsdocs build --clean --properties Configuration=Release --parameters fsdocs-package-version %s" prereleaseTag)
         "./"
 }
 
 let watchDocs = BuildTask.create "WatchDocs" [build] {
     printfn "watching docs with stable version %s" stableVersionTag
     runDotNet 
-        (sprintf "fsdocs watch --eval --clean --properties Configuration=Release --parameters fsdocs-package-version %s" stableVersionTag)
+        (sprintf "fsdocs watch --clean --properties Configuration=Release --parameters fsdocs-package-version %s" stableVersionTag)
         "./"
 }
 
 let watchDocsPrerelease = BuildTask.create "WatchDocsPrerelease" [setPrereleaseTag; build] {
     printfn "watching docs with prerelease version %s" prereleaseTag
     runDotNet 
-        (sprintf "fsdocs watch --eval --clean --properties Configuration=Release --parameters fsdocs-package-version %s" prereleaseTag)
+        (sprintf "fsdocs watch --clean --properties Configuration=Release --parameters fsdocs-package-version %s" prereleaseTag)
         "./"
 }
